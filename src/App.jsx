ceecfcf21a321678c155b0bf6cb71cbcc363d95a
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-05-07 02:54:55 -0400

    Created components and made imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import { useState } from 'react';
+import Menu from './components/Menu';
+import Categories from './components/Categories';
+import data from './data';
 
 function App() {
 
   return (
     <div>
-      
+
     </div>
   )
 }
