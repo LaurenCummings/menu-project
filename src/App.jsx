eed4f015d47605c951c3c929323694dc48f0eb7e
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-05-09 00:34:53 -0400

    Capitalized title

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
     <main>
       <section className="menu section">
         <div className="title">
-          <h2>Our menu</h2>
+          <h2>Our Menu</h2>
           <div className="underline"></div>
         </div>
         <Categories categories={categories} filterItems={filterItems} />
