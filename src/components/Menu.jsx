0bf21569d5f382366981698f09bf930232cd7ff1
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-05-09 00:42:56 -0400

    centered content and gave a width

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -9,7 +9,7 @@ function Menu({items}) {
                     <img src={img} alt={title} className="photo" />
                     <div className="item-info">
                         <header>
-                            <h4>{title}</h4>
+                            <h4 className="food-name">{title}</h4>
                             <h4 className="price">${price}</h4>
                         </header>
                         <p className="item-text">{desc}</p>
